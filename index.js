0a698bebaa9dce936c0dc0a39567959852ee0da1
Radovan Šurlák <radovan.surlak@gmail.com>
2020-12-12 13:53:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/download/:id', (req, res) => {
+  const { id } = req.params;
+
+  const URL = 'https://www.youtube.com/watch?v=lyrEsJLQa-c';
+
+  const stream = ytdl(URL, {
+    filter: 'audioonly',
+  });
+
+  stream.on('info', (videoInfo, formatInfo) => {
+    res.setHeader(
+      'Content-disposition',
+      `attachment; filename=${videoInfo.videoDetails.title}.${formatInfo.container}`,
+    );
+    res.setHeader('Content-type', `${formatInfo.mimeType}`);
+  });
+
+  stream.pipe(res);
+});
+
+app.listen(4000, () => {
+  console.log('Server IS ON !!! At port 4000');
+});
